a5563fcedefb981e780000c21541696963a1f95b
meannzen <senmeann@gmail.com>
2025-11-23 20:29:13 +0700

    Refactor

--- src/page.rs
+++ src/page.rs
@@ -2,15 +2,3 @@
 pub struct DbHeader {
     pub page_size: u32,
 }
-
-pub struct TableLeafPage {
-    pub header: PageHeader,
-}
-
-pub enum Page {
-    TableLeaf(TableLeafPage),
-}
-
-pub struct PageHeader {
-    cell_count: u16,
-}
